332486e4c3ef056b9c031791d0c893cff220aecf
“nkusikevin” <nkusikvnhart@gmail.com>
2021-08-04 23:31:37 +0200

    adding database remote connection

--- app/database/connect.php
+++ app/database/connect.php
@@ -1,11 +1,28 @@
 <?php
 
-$host = 'localhost';
-$user = 'root';
-$pass = '';
-$db_name = 'blog';
 
-$conn = new MySQLi($host, $user, $pass, $db_name);
+
+
+
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+
+
+// $host = 'localhost';
+// $user = 'root';
+// $pass = '';
+// $db_name = 'blog';
+
+// $conn = new MySQLi($host, $user, $pass, $db_name);
 
 if ($conn->connect_error) {
     die('Database connection error: ' . $conn->connect_error);
